21e95479a186807cff708947310d24020d5fe204
mgurner1234 <michaelgurner@hotmail.com>
2020-04-27 18:10:06 +0100

    [DEV_MGURNER_ZAIZI_TEST] Return a NO_CONTENT response fore DELETE

--- spring-boot-reactive-web/src/main/java/com/thepracticaldeveloper/reactiveweb/controller/QuoteDeleteController.java
+++ spring-boot-reactive-web/src/main/java/com/thepracticaldeveloper/reactiveweb/controller/QuoteDeleteController.java
@@ -1,6 +1,8 @@
 package com.thepracticaldeveloper.reactiveweb.controller;
 
 import com.thepracticaldeveloper.reactiveweb.repository.QuoteMongoBlockingRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +17,9 @@ public class QuoteDeleteController {
     }
 
     @DeleteMapping("/quotes-delete")
-    public void deleteQuote(final @RequestParam(name = "quoteId") String quoteId) {
+    public ResponseEntity<String> deleteQuote(final @RequestParam(name = "quoteId") String quoteId) {
         quoteMongoBlockingRepository.deleteById(quoteId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
